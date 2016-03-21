80dab878bd85196f27e3472133a8f4a0d87e48a0
OliviaZhang <sealife24@gmail.com>
2016-03-21 17:43:06 -0400

    add API endpoint to urls.py

--- googlePlus/settings.py
+++ googlePlus/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication'
+    'authentication',
+    'rest_framework_nested'
 ]
 
 MIDDLEWARE_CLASSES = [
