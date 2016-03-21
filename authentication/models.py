f1209ceb0d3997bbde78c89c5c0bb93ca81885d7
OliviaZhang <sealife24@gmail.com>
2016-03-21 13:20:17 -0400

    migrated the db and created super user admin

--- authentication/models.py
+++ authentication/models.py
@@ -3,33 +3,6 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager
 from django.db import models
 
-class Account(AbstractBaseUser):
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=40, unique=True)
-
-    first_name = models.CharField(max_length=40, blank=True)
-    last_name = models.CharField(max_length=40, blank=True)
-    tagline = models.CharField(max_length=140, blank=True)
-
-    is_admin = models.BooleanField(default=False)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    #objects = AccountManager()
-
-    USERNAME_FIELD = 'email'          # to tell django we want to use email to log user in
-    REQUIRED_FIELDS = ['username']    # since this model is not User, we need to tell Djano what is the required fields
-
-    def __unicode__(self):
-        return self.email
-
-    def get_full_name(self):
-        return ' '.join([self.first_name, self.last_name])
-
-    def get_short_name(self):
-        return self.first_name
-
 
 class AccountManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
@@ -55,3 +28,31 @@ class AccountManager(BaseUserManager):
         account.save()
 
         return account
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=40, unique=True)
+
+    first_name = models.CharField(max_length=40, blank=True)
+    last_name = models.CharField(max_length=40, blank=True)
+    tagline = models.CharField(max_length=140, blank=True)
+
+    is_admin = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'          # to tell django we want to use email to log user in
+    REQUIRED_FIELDS = ['username']    # since this model is not User, we need to tell Djano what is the required fields
+
+    def __unicode__(self):
+        return self.email
+
+    def get_full_name(self):
+        return ' '.join([self.first_name, self.last_name])
+
+    def get_short_name(self):
+        return self.first_name
+
