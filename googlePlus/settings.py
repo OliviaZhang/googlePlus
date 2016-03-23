db7f5ed59553cbe4745c251ac928703eba79f967
OliviaZhang <sealife24@gmail.com>
2016-03-23 17:29:29 -0400

    use mysql instead, migrated the db

--- googlePlus/settings.py
+++ googlePlus/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'googlePlus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'olivia',
+        'PASSWORD': '123123',
+        'NAME': 'mydb',
+        'HOST': 'localhost',
+        'PORT': '3360',
     }
 }
 
