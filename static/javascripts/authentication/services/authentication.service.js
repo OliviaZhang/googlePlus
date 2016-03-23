e4a82c78a00d1b617a62474364dc0f7584e5cce9
OliviaZhang <sealife24@gmail.com>
2016-03-23 11:02:52 -0400

    add static folder for angular files and templates. Added authentication service and register template

---
+++ static/javascripts/authentication/services/authentication.service.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('authentication.services')
+        .factory('Authentication', Authentication); // register a service authentication on the module
+
+    Authentication.$inject = ['$cookies', '$http']; // inject the 2 services as dependencies
+
+    function Authentication($cookies, $http) {
+        var Authentication = {
+            register:register // define your service as a named object and then return it
+        };
+
+        return Authentication;
+    }
+
+    function register(email, password, username) {
+        return $http.post('/api/v1/accounts/', {
+           username: username,
+           password: password,
+           email: email
+        });
+    }
+})
