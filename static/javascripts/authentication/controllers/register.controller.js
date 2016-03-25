0db34064201dfb829b3544ab266eb2048571758a
OliviaZhang <sealife24@gmail.com>
2016-03-25 17:13:58 -0400

    check to see if the user already logins on registration page

--- static/javascripts/authentication/controllers/register.controller.js
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -13,6 +13,14 @@
 
         vm.register = register;
 
+        activate();
+        
+        function activate() {
+            if (Authentication.isAuthenticated()) {
+                $location.url('/');
+            }
+        }
+
         function register() {
             Authentication.register(vm.email, vm.password, vm.username);
         }
