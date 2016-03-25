39bf8d108cf39f67e7f61be8cf65cdb174442c02
OliviaZhang <sealife24@gmail.com>
2016-03-25 16:26:20 -0400

    add login api view path

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,6 @@
+
+import json
+
 from rest_framework import permissions, viewsets
 
 from authentication.models import Account
@@ -7,6 +10,7 @@ from authentication.serializers import AccountSerializer
 from django.views.generic.base import TemplateView
 from rest_framework import status, views
 from rest_framework.response import Response
+from django.contrib.auth import authenticate, login
 
 class IndexView(TemplateView):
     template_name = '../templates/index.html'
@@ -37,3 +41,31 @@ class AccountViewSet(viewsets.ModelViewSet):
             'status': 'Bad request',
             'message': 'Account could not be created with received data.'
         }, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(views.APIView):
+    def post(self, request, format=None):
+        data = json.loads(request.body)
+
+        email = data.get('emails', None)
+        password = data.get('password', None)
+
+        account = authenticate(email=email, password=password)
+
+        if account is not None:
+            if account.is_active:
+                login(request, account)
+
+                serialized = AccountSerializer(account)
+
+                return Response(serialized.data)
+
+            else:
+                return Response({
+                    'status': 'Unauthorized',
+                    'message': 'This account has been disabled'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({
+                'status': 'Unauthorized',
+                'massage': 'Username/password combination invalid'
+            }, status=status.HTTP_401_UNAUTHORIZED)
--- googlePlus/urls.py
+++ googlePlus/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls import patterns, url, include
 
 from authentication.views import AccountViewSet
 
+from authentication.views import LoginView
+
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
 
@@ -13,6 +15,8 @@ urlpatterns = patterns(
     '',
 
     url(r'^api/v1/', include(router.urls)),
+
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
 
     url('^.*$', IndexView.as_view(), name='index'),
 )
