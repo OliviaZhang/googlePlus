bd0222dbd415a5df06d60096cb93018a591fa22e
OliviaZhang <sealife24@gmail.com>
2016-03-25 20:34:14 -0400

    add PostViewSet and AccountPostsViewSet API end point

--- googlePlus/urls.py
+++ googlePlus/urls.py
@@ -28,6 +28,8 @@ urlpatterns = patterns(
 
     url(r'^api/v1/', include(router.urls)),
 
+    url(r'^api/v1/', include(accounts_router.urls)),
+
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
 
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
